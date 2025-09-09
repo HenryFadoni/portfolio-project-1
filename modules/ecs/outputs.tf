2916938ea72ec7eb3ef88058b7828512ae937e8f
Henrique Fadoni <henriquefadonii@gmail.com>
2025-09-09 19:51:07 -0300

    Fix arn error

--- modules/ecs/outputs.tf
+++ modules/ecs/outputs.tf
@@ -25,7 +25,7 @@ output "service_name" {
 
 output "service_arn" {
   description = "ARN of the ECS service"
-  value       = aws_ecs_service.app.arn
+  value       = aws_ecs_service.app.id
 }
 
 output "task_definition_arn" {
--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -116,11 +116,19 @@ resource "aws_db_proxy" "main" {
   }
 }
 
+# RDS Proxy Default Target Group
+resource "aws_db_proxy_default_target_group" "main" {
+  count = var.enable_rds_proxy ? 1 : 0
+
+  db_proxy_name = aws_db_proxy.main[0].name
+}
+
 # RDS Proxy Target
 resource "aws_db_proxy_target" "main" {
   count = var.enable_rds_proxy ? 1 : 0
 
   db_proxy_name          = aws_db_proxy.main[0].name
+  target_group_name      = aws_db_proxy_default_target_group.main[0].name
   db_instance_identifier = aws_db_instance.main.identifier
 }
 
